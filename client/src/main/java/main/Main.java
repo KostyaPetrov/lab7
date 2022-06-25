2078d62b29af5c68ebb083634a8b915c574ec275
KostyaPetrov <pki04@mail.ru>
2022-06-25 10:39:33 +0300

    last

---
+++ client/src/main/java/main/Main.java
@@ -0,0 +1,48 @@
+package main;
+
+
+import java.io.PrintStream;
+
+import client.Client;
+import common.exceptions.*;
+
+import static common.io.OutputManager.*;
+
+
+public class Main {
+    //public static Logger logger = LogManager.getLogger("logger");
+    //static final Logger logger = LogManager.getRootLogger();
+    public static void main(String[] args) throws Exception {
+
+
+        String address = "localhost";
+        String strPort = "4445";
+        int port = 0;
+        try {
+            if (args.length == 2) {
+                address = args[0];
+                strPort = args[1];
+            }
+            if(args.length == 1){
+                strPort = args[0];
+                print("no address passed by arguments, setting default : " + address);
+            }
+            if(args.length == 0){
+                print("no port and no address passed by arguments, setting default :" + address + "/" + strPort);
+            }
+            try {
+                port = Integer.parseInt(strPort);
+            } catch (NumberFormatException e) {
+                throw new InvalidPortException();
+            }
+            Client client = new Client(address, port);
+            try {
+                client.start();
+            } catch (EndOfInputException e) {
+                printErr(e.getMessage());
+            }
+        } catch (ConnectionException e) {
+            print(e.getMessage());
+        }
+    }
+}
